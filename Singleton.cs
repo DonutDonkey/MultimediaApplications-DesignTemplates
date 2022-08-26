0ad328cbf17557c4b9289ef658e20da90b047492
KGM <34782950+DonutDonkey@users.noreply.github.com>
2022-08-26 20:23:20 +0200

    Create Singleton.cs

---
+++ Singleton.cs
@@ -0,0 +1,18 @@
+
+using UnityEngine;
+
+namespace Script.Mono {
+    public class Singleton<T> : MonoBehaviour where T : Component {
+        public static T Instance { get; private set; }
+
+        protected virtual void Awake() {
+            if (Instance == null) {
+                Debug.Log(GetType().Name + "- Instance Created");
+                Instance = this as T;
+            } else {
+                Debug.Log(GetType().Name + "- Instance Destroyed");
+                Destroy(gameObject);
+            }
+        }
+    }
+}
